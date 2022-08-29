dcdb3753fc3cf5d255b16d50ef4bad498b1cba6b
xVieiraLucas <lucas.brazilianpilo@gmail.com>
2022-08-29 17:46:56 -0300

    aa

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -5,13 +5,13 @@
 //console.log(litros)
 //console.log(beba)
 
-const perguntaAgua = "Você já bebeu água hoje ?"
-const respostaAgua = prompt(perguntaAgua)
-const perguntaLitros = "mais do que 1L ?"
-const repostaLitros = prompt(perguntaLitros)
-const perguntaBeba = "irá beber mais ?"
-const respostaBeba = prompt(perguntaBeba)
+const perguntaAgua = "Você já bebeu água hoje ?";
+const respostaAgua = prompt(perguntaAgua);
+const perguntaLitros = "mais do que 1L ?";
+const repostaLitros = prompt(perguntaLitros);
+const perguntaBeba = "irá beber mais ?";
+const respostaBeba = prompt(perguntaBeba);
 
-console.log(perguntaAgua, respostaAgua)
-console.log(perguntaLitros, repostaLitros)
-console.log(perguntaBeba, respostaBeba)
+console.log(perguntaAgua, respostaAgua);
+console.log(perguntaLitros, repostaLitros);
+console.log(perguntaBeba, respostaBeba);
