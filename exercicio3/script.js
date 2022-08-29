8d62bdf3c1a71c0892e77409ab4dc0069a005405
xVieiraLucas <lucas.brazilianpilo@gmail.com>
2022-08-29 17:43:25 -0300

    Exercicio completo corrigido Lucas Vieira

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -7,6 +7,6 @@ console.log(a, b)
 let c = a
 a = b
 b = c
-console.log("o resultado de A é ", b)
-console.log("o resultado de B é", a)
+console.log("o resultado de A é ", a)
+console.log("o resultado de B é", b)
 console.log(a, b)
