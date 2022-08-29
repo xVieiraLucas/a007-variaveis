6bb51f08100d041c3c5e9251c671fba3ebe2918f
xVieiraLucas <lucas.brazilianpilo@gmail.com>
2022-08-29 14:42:35 -0300

    exericio 2

---
+++ exercicio2/script.js
@@ -0,0 +1,17 @@
+//let agua = confirm("você já bebeu água hoje ?")
+//let litros = confirm("mais do que 1L ?")
+//let beba = confirm("irá beber mais ?")
+//console.log(agua)
+//console.log(litros)
+//console.log(beba)
+
+const perguntaAgua = "Você já bebeu água hoje ?"
+const respostaAgua = prompt(perguntaAgua)
+const perguntaLitros = "mais do que 1L ?"
+const repostaLitros = prompt(perguntaLitros)
+const perguntaBeba = "irá beber mais ?"
+const respostaBeba = prompt(perguntaBeba)
+
+console.log(perguntaAgua, respostaAgua)
+console.log(perguntaLitros, repostaLitros)
+console.log(perguntaBeba, respostaBeba)
